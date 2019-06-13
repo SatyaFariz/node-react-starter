421b1cc5dca5fa8bea7e9579b34383a893ffc133
satyafariz <satyafariznur@gmail.com>
2019-06-14 00:17:01 +0700

    add update user name functionality

--- server/mutations/UserMutation.js
+++ server/mutations/UserMutation.js
@@ -1,11 +1,14 @@
 const {
   GraphQLObjectType,
-  GraphQLNonNull
+  GraphQLNonNull,
+  GraphQLString
 } = require('graphql')
 
+const User = require('../model/User')
 const UserInput = require('../model/input/UserInput')
 const SignUpPayload = require('../model/payload/SignUpPayload')
 const userSignUp = require('../utils/mutation-resolvers/userSignUp')
+const userNameUpdate = require('../utils/mutation-resolvers/userNameUpdate')
 
 module.exports = new GraphQLObjectType({
   name: 'UserMutation',
@@ -16,6 +19,14 @@ module.exports = new GraphQLObjectType({
         input: { type: new GraphQLNonNull(UserInput) }
       },
       resolve: userSignUp
+    },
+
+    name_update: {
+      type: User,
+      args: {
+        name: { type: new GraphQLNonNull(GraphQLString) }
+      },
+      resolve: userNameUpdate
     }
   })
 })
