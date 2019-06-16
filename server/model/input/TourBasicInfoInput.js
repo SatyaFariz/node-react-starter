a1441a1a83e48628e8ab59cbb621bab336824e5e
satyafariz <satyafariznur@gmail.com>
2019-06-16 22:00:44 +0700

    renamed a file & add TourBasicInfoInput

---
+++ server/model/input/TourBasicInfoInput.js
@@ -0,0 +1,45 @@
+const {
+  GraphQLString,
+  GraphQLInt,
+  GraphQLFloat,
+  GraphQLBoolean,
+  GraphQLNonNull,
+  GraphQLInputObjectType
+} = require('graphql')
+
+module.exports = new GraphQLInputObjectType({
+  name: 'SignInInput',
+  fields: () => ({
+    province: {
+      type: new GraphQLNonNull(GraphQLString)
+    },
+
+    city: {
+      type: new GraphQLNonNull(GraphQLString)
+    },
+
+    name: {
+      type: new GraphQLNonNull(GraphQLString)
+    },
+
+    duration_in_days: {
+      type: new GraphQLNonNull(GraphQLInt)
+    },
+
+    price_per_person: {
+      type: new GraphQLNonNull(GraphQLFloat)
+    },
+
+    free_cancellation: {
+      type: new GraphQLNonNull(GraphQLBoolean)
+    },
+
+    description: {
+      type: new GraphQLNonNull(GraphQLString)
+    },
+
+    good_for: {
+      type: new GraphQLNonNull(GraphQLString)
+    },
+  })
+})
