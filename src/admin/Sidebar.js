2820d292b6c911f3a0f2179d700b702dc410f0c3
satyafariz <satyafariznur@gmail.com>
2019-06-30 01:43:46 +0700

    add some links

--- src/admin/Sidebar.js
+++ src/admin/Sidebar.js
@@ -1,6 +1,11 @@
 import React from 'react'
 import { makeStyles } from '@material-ui/core/styles'
 import Drawer from '@material-ui/core/Drawer'
+import ListItem from '@material-ui/core/ListItem'
+import ListItemText from '@material-ui/core/ListItemText'
+import List from '@material-ui/core/List'
+
+import Link from '../common/Link'
 
 const drawerWidth = 240
 
@@ -31,6 +36,14 @@ const Component = props => {
       anchor="left"
     >
       <div className={c.toolbar}/>
+      <List>
+        <ListItem component={Link} button href="/admin">
+          <ListItemText primary={'Home'} />
+        </ListItem>
+        <ListItem component={Link} button href="/admin/tours">
+          <ListItemText primary={'Tours'} />
+        </ListItem>
+      </List>
     </Drawer>
   )
 }
