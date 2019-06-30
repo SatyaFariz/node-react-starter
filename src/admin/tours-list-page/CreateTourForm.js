9f8af64db7d204c1a850e98c3b9ebd022232f598
satyafariz <satyafariznur@gmail.com>
2019-06-30 23:26:27 +0700

    add form validation

--- src/admin/tours-list-page/CreateTourForm.js
+++ src/admin/tours-list-page/CreateTourForm.js
@@ -3,11 +3,17 @@ import { makeStyles } from '@material-ui/core/styles'
 import Paper from '@material-ui/core/Paper'
 import TextField from '@material-ui/core/TextField'
 import MenuItem from '@material-ui/core/MenuItem'
+import Button from '@material-ui/core/Button'
+
+import Validator from '../../utils/validator'
 
 const useStyles = makeStyles(theme => ({
   container: {
     padding: 15,
     width: 500
+  },
+  button: {
+    marginTop: 20
   }
 }))
 
@@ -33,52 +39,127 @@ const Component = props => {
     good_for: ''
   })
 
- // const [validation, setValidation] = useState({ isValid: false })
+  const [validation, setValidation] = useState({ isValid: false })
 
   const handleChange = name => event => {
     setInput({ ...input, [name]: event.target.value })
   }
 
+  const isZeroOrNegative = (string) => {
+    const number = parseInt(string, 10)
+    return number <= 0
+  }
+
+  const validate = () => {
+    const validator = new Validator([
+      {
+        field: 'province',
+        method: Validator.isEmpty,
+        validWhen: false,
+        message: 'This field is required.'
+      },
+      {
+        field: 'city',
+        method: Validator.isEmpty,
+        validWhen: false,
+        message: 'This field is required.'
+      },
+      {
+        field: 'display_location',
+        method: Validator.isEmpty,
+        validWhen: false,
+        message: 'This field is required.'
+      },
+      {
+        field: 'name',
+        method: Validator.isEmpty,
+        validWhen: false,
+        message: 'This field is required.'
+      },
+      {
+        field: 'duration_in_days',
+        method: Validator.isEmpty,
+        validWhen: false,
+        message: 'This field is required.'
+      },
+      {
+        field: 'duration_in_days',
+        method: isZeroOrNegative,
+        validWhen: false,
+        message: 'This field must be greater than 0.'
+      },
+      {
+        field: 'price_per_person',
+        method: Validator.isEmpty,
+        validWhen: false,
+        message: 'This field is required.'
+      },
+      {
+        field: 'price_per_person',
+        method: isZeroOrNegative,
+        validWhen: false,
+        message: 'This field must be greater than 0.'
+      },
+      {
+        field: 'description',
+        method: Validator.isEmpty,
+        validWhen: false,
+        message: 'This field is required.'
+      }
+    ])
+
+    const validation = validator.validate(input)
+    setValidation(validation)
+    return validation
+  }
+
+  const save = () => {
+    const validation = validate()
+    if(validation.isInvalid) {
+      console.log('')
+    }
+  }
+
   return (
     <Paper className={c.container}>
       <TextField
-        label="Province"
+        label="*Province"
         defaultValue={input.province}
         onChange={handleChange('province')}
         margin="normal"
         fullWidth
-   //     error={validation.username && validation.username.isInvalid}
-    //    helperText={validation.username && validation.username.message}
+        error={validation.province && validation.province.isInvalid}
+        helperText={validation.province && validation.province.message}
       />
 
       <TextField
-        label="City"
+        label="*City"
         defaultValue={input.name}
         onChange={handleChange('city')}
         margin="normal"
         fullWidth
-      //  error={validation.username && validation.username.isInvalid}
-      //  helperText={validation.username && validation.username.message}
+        error={validation.city && validation.city.isInvalid}
+        helperText={validation.city && validation.city.message}
       />
 
       <TextField
-        label="Display Location"
+        label="*Display Location"
         defaultValue={input.name}
         onChange={handleChange('display_location')}
         margin="normal"
         fullWidth
-      //  error={validation.username && validation.username.isInvalid}
-      //  helperText={validation.username && validation.username.message}
+        error={validation.display_location && validation.display_location.isInvalid}
+        helperText={validation.display_location && validation.display_location.message}
       />
 
       <TextField
-        label="Name"
+        label="*Name"
         defaultValue={input.name}
         onChange={handleChange('name')}
         margin="normal"
         fullWidth
-      //  error={validation.username && validation.username.isInvalid}
-      //  helperText={validation.username && validation.username.message}
+        error={validation.name && validation.name.isInvalid}
+        helperText={validation.name && validation.name.message}
       />
 
       <TextField
@@ -87,39 +168,38 @@ const Component = props => {
         onChange={handleChange('category')}
         margin="normal"
         fullWidth
-      //  error={validation.username && validation.username.isInvalid}
-      //  helperText={validation.username && validation.username.message}
       />
 
       <TextField
-        label="Duration (days)"
+        label="*Duration (days)"
         defaultValue={input.name}
         onChange={handleChange('duration_in_days')}
         margin="normal"
         fullWidth
-      //  error={validation.username && validation.username.isInvalid}
-      //  helperText={validation.username && validation.username.message}
+        type="number"
+        error={validation.duration_in_days && validation.duration_in_days.isInvalid}
+        helperText={validation.duration_in_days && validation.duration_in_days.message}
       />
 
       <TextField
-        label="Price/Person"
+        label="*Price/Person"
         defaultValue={input.name}
         onChange={handleChange('price_per_person')}
         margin="normal"
         type="number"
         fullWidth
-      //  error={validation.username && validation.username.isInvalid}
-      //  helperText={validation.username && validation.username.message}
+        error={validation.price_per_person && validation.price_per_person.isInvalid}
+        helperText={validation.price_per_person && validation.price_per_person.message}
       />
 
       <TextField
-        label="Description"
+        label="*Description"
         defaultValue={input.name}
         onChange={handleChange('description')}
         margin="normal"
         fullWidth
-      //  error={validation.username && validation.username.isInvalid}
-      //  helperText={validation.username && validation.username.message}
+        error={validation.description && validation.description.isInvalid}
+        helperText={validation.description && validation.description.message}
       />
 
       <TextField
@@ -127,7 +207,6 @@ const Component = props => {
         label="Free Cancellation"
         value={input.free_cancellation}
         onChange={handleChange('free_cancellation')}
-    //    helperText="Please select your currency"
         margin="normal"
         fullWidth
       >
@@ -141,8 +220,6 @@ const Component = props => {
         onChange={handleChange('foods_included')}
         margin="normal"
         fullWidth
-      //  error={validation.username && validation.username.isInvalid}
-      //  helperText={validation.username && validation.username.message}
       />
 
       <TextField
@@ -151,8 +228,6 @@ const Component = props => {
         onChange={handleChange('drinks_included')}
         margin="normal"
         fullWidth
-      //  error={validation.username && validation.username.isInvalid}
-      //  helperText={validation.username && validation.username.message}
       />
 
       <TextField
@@ -161,8 +236,6 @@ const Component = props => {
         onChange={handleChange('accomodations_included')}
         margin="normal"
         fullWidth
-      //  error={validation.username && validation.username.isInvalid}
-      //  helperText={validation.username && validation.username.message}
       />
 
       <TextField
@@ -171,8 +244,6 @@ const Component = props => {
         onChange={handleChange('tickets_included')}
         margin="normal"
         fullWidth
-      //  error={validation.username && validation.username.isInvalid}
-      //  helperText={validation.username && validation.username.message}
       />
 
       <TextField
@@ -181,8 +252,6 @@ const Component = props => {
         onChange={handleChange('transportation_included')}
         margin="normal"
         fullWidth
-      //  error={validation.username && validation.username.isInvalid}
-      //  helperText={validation.username && validation.username.message}
       />
 
       <TextField
@@ -191,8 +260,6 @@ const Component = props => {
         onChange={handleChange('equipment_included')}
         margin="normal"
         fullWidth
-      //  error={validation.username && validation.username.isInvalid}
-      //  helperText={validation.username && validation.username.message}
       />
 
       <TextField
@@ -201,9 +268,17 @@ const Component = props => {
         onChange={handleChange('good_for')}
         margin="normal"
         fullWidth
-      //  error={validation.username && validation.username.isInvalid}
-      //  helperText={validation.username && validation.username.message}
       />
+
+      <Button
+        color="primary"
+        variant="contained"
+        onClick={save}
+        className={c.button}
+        fullWidth
+      >
+        Save
+      </Button>
     </Paper>
   )
 }
