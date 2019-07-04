d6a9dceebabf38ed27be3af32f00ec4de95408e3
satyafariz <satyafariznur@gmail.com>
2019-07-05 00:12:42 +0700

    add edit form

--- src/admin/tour-detail-page/DescriptionEdit.js
+++ src/admin/tour-detail-page/DescriptionEdit.js
@@ -1,5 +1,9 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { makeStyles } from '@material-ui/core/styles'
+import TextField from '@material-ui/core/TextField'
+import FormActionButtons from './FormActionButtons'
+import Validator from '../../utils/validator'
+
 
 const useStyles = makeStyles(theme => ({
   container: {
@@ -10,9 +14,48 @@ const useStyles = makeStyles(theme => ({
 const Component = props => {
   const c = useStyles()
 
+  const [description, setDescription] = useState('')
+
+  const [validation, setValidation] = useState({ isValid: false })
+
+  const validate = () => {
+    const validator = new Validator([
+      {
+        field: 'description',
+        method: Validator.isEmpty,
+        validWhen: false,
+        message: 'This field is required.'
+      }
+    ])
+
+    const validation = validator.validate({ description })
+    setValidation(validation)
+    return validation
+  }
+
+  const handleChange = ({ target: { value }}) => setDescription(value)
+
+  const save = () => {
+    validate()
+  }
+
   return (
-    <div className={c.container} onClick={props.closeEdit}>
-      Cancel
+    <div className={c.container}>
+      <TextField
+        label="*Description"
+        defaultValue={description}
+        onChange={handleChange}
+        margin="normal"
+        multiline
+        fullWidth
+        error={validation.description && validation.description.isInvalid}
+        helperText={validation.description && validation.description.message}
+      />
+
+      <FormActionButtons
+        onSaveButtonClick={save}
+        onCancelButtonClick={props.closeEdit}
+      />
     </div>
   )
 }
