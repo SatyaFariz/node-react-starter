1abf8be3f906f298ab7e565e55a815c2cdbfe695
satyafariz <satyafariznur@gmail.com>
2019-07-05 17:39:39 +0700

    add delete button

--- src/admin/tour-detail-page/SectionHeader.js
+++ src/admin/tour-detail-page/SectionHeader.js
@@ -3,12 +3,17 @@ import { makeStyles } from '@material-ui/core/styles'
 import Typography from '@material-ui/core/Typography'
 import IconButton from '@material-ui/core/IconButton'
 import EditButton from '@material-ui/icons/Edit'
+import DeleteButton from '@material-ui/icons/Delete'
 
 const useStyles = makeStyles(theme => ({
   container: {
     display: 'flex',
     justifyContent: 'space-between',
     alignItems: 'center'
+  },
+
+  deleteButton: {
+    marginLeft: theme.spacing(1)
   }
 }))
 
@@ -24,6 +29,12 @@ const Component = props => {
       {props.showEditButton &&
       <IconButton onClick={props.onEditButtonClick}>
         <EditButton size="small"/>
+      </IconButton>
+      }
+
+      {props.showDeleteButton &&
+      <IconButton onClick={props.onDeleteButtonClick} className={c.deleteButton}>
+        <DeleteButton size="small"/>
       </IconButton>
       }
     </div>
