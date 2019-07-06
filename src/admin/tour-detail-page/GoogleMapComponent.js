3222e384a14a5055dabeba9776eac81b040f0dd0
satyafariz <satyafariznur@gmail.com>
2019-07-07 01:17:58 +0700

    add search box

--- src/admin/tour-detail-page/GoogleMapComponent.js
+++ src/admin/tour-detail-page/GoogleMapComponent.js
@@ -1,7 +1,27 @@
 import React from 'react'
+import { makeStyles } from '@material-ui/core/styles'
 import { withScriptjs, withGoogleMap, GoogleMap, Marker } from "react-google-maps"
+import SearchBox from "react-google-maps/lib/components/places/SearchBox"
+
+const useStyles = makeStyles(theme => ({
+  input: {
+    boxSizing: `border-box`,
+    border: `1px solid transparent`,
+    width: `500px`,
+    height: `40px`,
+    marginTop: `10px`,
+    padding: `0 12px`,
+    borderRadius: `2px`,
+    boxShadow: `rgba(0, 0, 0, 0.3) 0px 1px 4px -1px`,
+    fontSize: `14px`,
+    outline: `none`,
+    fontFamily: theme.typography.fontFamily
+  }
+}))
 
 const Component = props => {
+  const c = useStyles()
+
   const {
     center,
     markerDraggable,
@@ -20,6 +40,18 @@ const Component = props => {
       defaultCenter={center}
       center={center}
     >
+      <SearchBox
+      //  ref={props.onSearchBoxMounted}
+      //  bounds={props.bounds}
+        controlPosition={window.google.maps.ControlPosition.TOP_LEFT}
+      //  onPlacesChanged={props.onPlacesChanged}
+      >
+        <input
+          type="text"
+          placeholder="Search..."
+          className={c.input}
+        />
+      </SearchBox>
       {isMarkerShown && 
         <Marker 
           draggable={markerDraggable}
