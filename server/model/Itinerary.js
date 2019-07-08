2c48e043575df49c0e99ac4baee805eab12cf67d
satyafariz <satyafariznur@gmail.com>
2019-07-08 14:31:34 +0700

    add id field

--- server/model/Itinerary.js
+++ server/model/Itinerary.js
@@ -6,6 +6,11 @@ const {
 module.exports = new GraphQLObjectType({
   name: 'Itinerary',
   fields: () => ({
+    id: {
+      type: GraphQLString,
+      resolve: root => root._id.toString()
+    },
+
     time_description: {
       type: GraphQLString
     },
