fdf0bd5a16ddec7a97fb828611d8faa53b1fb958
satyafariz <satyafariznur@gmail.com>
2019-07-17 02:14:15 +0700

    add display image upload functionality

--- server/utils/mutation-resolvers/adminTourDisplayImageUpload.js
+++ server/utils/mutation-resolvers/adminTourDisplayImageUpload.js
@@ -1,18 +1,47 @@
 const Tour = require('../../database/model/Tour')
 const { ObjectId } = require('mongoose').Types
+const cloudinary = require('../../lib/cloudinary')
 
-module.exports = async (root, { _id }) => {
-  const display_image = {
-    public_id: 'jdklfs',
-    url: 'https://test/tes',
-    secure_url: 'https://test/tes',
-    width: 1000,
-    height: 1000
-  }
+module.exports = async (root, { _id }, { session, req }) => {
+  const { admin } = session
+  const { files } = req
+  const file = files && files[0]
 
-  const updateStatus = await Tour.updateOne({ _id: ObjectId(_id) }, { display_image })
-  if(updateStatus.nModified > 0) {
+  if(!admin)
+    throw new Error("You're not authorized as admin")
 
-    return display_image
+  if(!file)
+    throw new Error('File is required.')
+
+  const { uploader } = cloudinary.v2
+
+  const uploaded = await uploader.upload(file.path)
+
+  if(uploaded) {
+    const display_image = {
+      public_id: uploaded.public_id,
+      url: uploaded.url,
+      secure_url: uploaded.secure_url,
+      width: uploaded.width,
+      height: uploaded.height
+    }
+
+    let doc = null
+
+    try {
+      doc = await Tour.findOneAndUpdate({ _id: ObjectId(_id) }, { display_image })
+
+      if(doc) {
+        return display_image
+      } else {
+        throw new Error()
+      }
+    } catch(err) {
+      uploader.destroy(uploaded.public_id)
+    } finally {
+      if(doc && doc.display_image) {
+        uploader.destroy(doc.display_image.public_id)
+      }
+    }
   }
 }
--- server/utils/mutation-resolvers/adminTourImageUpload.js
+++ server/utils/mutation-resolvers/adminTourImageUpload.js
@@ -1,7 +1,19 @@
 const Tour = require('../../database/model/Tour')
 const { ObjectId } = require('mongoose').Types
 
-module.exports = async (root, { _id }) => {
+module.exports = async (root, { _id }, { session, req }) => {
+  const { admin } = session
+  const { files } = req
+  const file = files && files[0]
+
+  if(!admin)
+    throw new Error("You're not authorized as admin")
+
+  if(!file)
+    throw new Error('File is required.')
+
+  console.log(file)  
+
   const image = {
     public_id: 'jdklfs',
     url: 'https://test/tes',
